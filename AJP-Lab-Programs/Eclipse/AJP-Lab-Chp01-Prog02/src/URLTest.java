80d363ef4748b48e4a8d0f381527a7bfc817e0cf
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-04 15:26:02 +0530

    Lab Programs updated

---
+++ AJP-Lab-Programs/Eclipse/AJP-Lab-Chp01-Prog02/src/URLTest.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+public class URLTest
+{
+public static void main(String[] args)
+{
+try 
+{
+URL url = new URL("https://marwadieducation.edu.in/MEFOnline/");
+// Also try "https://emp.marwadiuniversity.ac.in:553/ControlPages/AttendanceControl.aspx"
+
+System.out.println("Protocol: "+url.getProtocol());
+System.out.println("Host Name: "+url.getHost());
+System.out.println("Port Number: "+url.getPort());
+System.out.println("File Name with Path: "+url.getFile());
+}
+catch(Exception e){System.out.println(e);}
+}
+} 
