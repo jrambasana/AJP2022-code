802929510c3df159a55a020b410c08c7393b1a48
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-04 15:42:22 +0530

    AJP-Lab-Chp01-Prog02 updated

---
+++ AJP-Lab-Programs/NetBeans Programs/AJP-Lab-Chp01-Prog02/src/ajp/lab/chp01/prog02/URLTest.java
@@ -0,0 +1,26 @@
+package ajp.lab.chp01.prog02;
+
+import java.io.*;
+import java.net.*;
+public class URLTest
+{
+	public static void main(String[] args)
+	{
+		try 
+		{
+			String str = "https://marwadieducation.edu.in/MEFOnline/";
+			// Also try "https://emp.marwadiuniversity.ac.in:553/ControlPages/AttendanceControl.aspx"
+			
+			URL url = new URL(str);
+			
+			System.out.println("Protocol: "+url.getProtocol());
+			System.out.println("Host Name: "+url.getHost());
+			System.out.println("Port Number: "+url.getPort());
+			System.out.println("File Name with Path: "+url.getFile());
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+} 
