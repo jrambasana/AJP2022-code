f1bc7c884fa58858569dd1e730ea9e0410aa4df9
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-04 16:01:41 +0530

    AJP-Lab-Chp01-Prog03 added

---
+++ AJP-Lab-Programs/Eclipse/AJP-Lab-Chp01-Prog03/src/MyClient.java
@@ -0,0 +1,28 @@
+import java.net.*;   
+import java.io.*;  
+class MyClient
+{  
+	public static void main(String args[])throws Exception
+	{  
+		Socket s=new Socket("localhost",3333);  
+		System.out.println("Client started.");  
+		
+		DataInputStream din=new DataInputStream(s.getInputStream());  
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+		  
+		String str="",str2="";  
+		while(!str.equals("stop"))
+		{
+                    System.out.print("Enter your message here: ");  
+                    str=br.readLine();  
+                    dout.writeUTF(str);  
+                    dout.flush();  
+                    System.out.println("Waiting for Server's messsage...");  
+                    str2=din.readUTF();  
+                    System.out.println("Server says: "+str2);  
+		}   
+		dout.close();  
+		s.close();  
+	}
+}
