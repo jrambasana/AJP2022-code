7a1c70445b22771c8e00ab65d15d4de2bf466f8a
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-04 16:24:57 +0530

    AJP-Lab-Chp01-Prog03 added

---
+++ AJP-Lab-Programs/Eclipse/AJP-Lab-Chp01-Prog04/src/FileClient.java
@@ -0,0 +1,36 @@
+//Write	an application	which will retrieve file from server machine	and save that file on client machine.
+// File name will be provided by client.
+import java.io.*; 
+import java.net.*;
+class FileClient
+{
+	public static void main(String args[ ]) throws Exception
+	{
+		Socket s = new Socket("localhost", 8888);
+		BufferedReader kb = new BufferedReader(new
+		InputStreamReader(System.in));
+		System.out.print("Enter filename: ");
+		String fname = kb.readLine();
+		
+		DataOutputStream out = new 
+		DataOutputStream(s.getOutputStream());
+		out.writeBytes(fname+"\n");
+		BufferedReader  in = new BufferedReader(new
+		InputStreamReader(s.getInputStream()));
+		
+		String str;
+		str = in.readLine();
+		
+		if(str.equals("Yes"))
+		{    
+			while((str = in.readLine()) != null)
+				System.out.println(str);
+			kb.close();
+			out.close();
+			in.close();
+			s.close();
+		}
+		else 
+			System.out.println("File not found");
+	}
+} 
