d1cbfcdfcebc6f6fe782cbf7746e8bad9455f179
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-04 14:05:07 +0530

    InetAddressTest updated

--- NetBeans Programs/AJP-Prog01/src/ajp/prog01/InetAddressTest.java
+++ NetBeans Programs/AJP-Prog01/src/ajp/prog01/InetAddressTest.java
@@ -9,5 +9,10 @@ public class InetAddressTest
         InetAddress Address = InetAddress.getLocalHost();
         System.out.println("Localhost IP address: " + Address);
 
+        Address = InetAddress.getByName("www.google.com");
+        System.out.println("Google IP address: "+ Address);
+        InetAddress SW[] = InetAddress.getAllByName("www.yahoo.com");
+        for (int i=0; i<SW.length; i++)
+            System.out.println("Yahoo IP address: " + SW[i]);        
     }    
 }
