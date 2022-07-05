d1c1b24d5237fab64536d66faadff6b0ec39e382
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-05 12:09:51 +0530

    AJP-Lab-Chp01-Prog03 added

---
+++ AJP-Lab-Programs/NetBeans Programs/AJP-Lab-Chp01-Prog03/src/ajp/lab/chp01/prog03/MyServer.java
@@ -0,0 +1,36 @@
+// Write a two – way network based chat application. 
+// It will use TCP/IP protocol and it will do communication in serial manner.
+
+package ajp.lab.chp01.prog03;
+
+import java.net.*;  
+import java.io.*;  
+
+public class MyServer
+{  
+	public static void main(String args[])throws Exception
+	{  
+		ServerSocket ss=new ServerSocket(3333);  
+		System.out.println("Server started.");  
+
+		Socket s=ss.accept();  
+		DataInputStream din=new DataInputStream(s.getInputStream());  
+		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+		BufferedReader br=new BufferedReader(new    
+		InputStreamReader(System.in));  
+		String str="",str2="";  
+		while(!str.equals("stop"))
+		{  
+                        System.out.println("Waiting for Client's messsage...");  
+			str=din.readUTF();  
+			System.out.println("Client says: "+str);  
+                        System.out.print("Enter your message here: ");  
+			str2=br.readLine();  
+			dout.writeUTF(str2);  
+			dout.flush();  
+		}  
+		din.close();  
+		s.close();  
+		ss.close();  
+	}
+}
