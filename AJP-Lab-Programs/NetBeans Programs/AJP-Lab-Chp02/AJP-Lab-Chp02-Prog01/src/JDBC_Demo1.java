f1f3e2b2adbf420c3e380a8c3115d3fa39ca23b8
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-25 08:35:46 +0530

    programs updated

---
+++ AJP-Lab-Programs/NetBeans Programs/AJP-Lab-Chp02/AJP-Lab-Chp02-Prog01/src/JDBC_Demo1.java
@@ -0,0 +1,20 @@
+/**
+ *
+ * @author Jatin Ambasana
+ */
+import java.sql.*;
+public class JDBC_Demo1 {
+public static void main(String[] args) {
+  try {
+    //1. Register the driver
+    Class.forName("com.mysql.jdbc.Driver");
+
+    //2. establish connection by con object
+    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc","root","");
+
+    System.out.println("Connection established");
+
+} catch (Exception e) 
+{    System.out.println(e);     }
+}
+}
