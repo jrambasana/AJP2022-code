71686ec19d806e1959851cac10fc923774ad8dbf
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-25 12:04:42 +0530

    Update JDBC_Demo1.java

--- AJP-Lab-Programs/NetBeans Programs/AJP-Lab-Chp02/AJP-Lab-Chp02-Prog01/src/JDBC_Demo1.java
+++ AJP-Lab-Programs/NetBeans Programs/AJP-Lab-Chp02/AJP-Lab-Chp02-Prog01/src/JDBC_Demo1.java
@@ -15,20 +15,6 @@ public static void main(String[] args) {
 
     System.out.println("Connection established");
 
-    //3. Create the statement object which is 
-    //used to execute query in database
-    Statement stmt = con.createStatement(); 
-    
-    //step4 execute query  
-    ResultSet rs = stmt.executeQuery("select * from emp"); 
-    
-    System.out.println("DATA FETCHED: ");
-    while(rs.next())  
-    System.out.println(rs.getInt(1)+"  "+rs.getString(2)
-            +"  "+rs.getInt(3)+"  "+rs.getString(4));  
-    
-    //5.close the connection
-    con.close();
     
 } catch (Exception e) {
     System.out.println(e);
