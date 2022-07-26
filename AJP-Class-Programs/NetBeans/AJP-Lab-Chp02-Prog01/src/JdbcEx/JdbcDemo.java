4710925a1134a5d94022391f697a7e80f2ba2f1f
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-07-26 12:21:21 +0530

    updated

---
+++ AJP-Class-Programs/NetBeans/AJP-Lab-Chp02-Prog01/src/JdbcEx/JdbcDemo.java
@@ -0,0 +1,44 @@
+package JdbcEx;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Jatin Ambasana
+ */
+public class JdbcDemo {
+
+  static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+  static final String DB_URL = "jdbc:mysql://localhost/db1";
+  static final String USER = "root";
+  static final String PASS = "";
+
+  public static void main(String[] args) 
+  {
+    Connection conn = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+    try {
+      Class.forName(JDBC_DRIVER);
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      stmt = conn.createStatement();
+      String sql = "SELECT id, first_name, last_name, salary FROM employee";
+      rs = stmt.executeQuery(sql);
+      while (rs.next()) {
+        int id = rs.getInt("id");
+        int sal = rs.getInt("salary");
+        String first = rs.getString("first_name");
+        String last = rs.getString("last_name");
+
+        System.out.print("ID: " + id);
+        System.out.print(", Salary: " + sal);
+        System.out.print(", First Name: " + first);
+        System.out.println(", Last : " + last);
+        } 
+      stmt.close();
+      conn.close();
+    } catch (Exception e) {
+          System.out.println(e);
+    }
+  }   
+}
