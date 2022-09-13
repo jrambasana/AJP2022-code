c7a4a1b3f22f901f27f6334b1c4b5cf32bbfc961
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-09-13 13:19:38 +0530

    Q042 created

---
+++ AJP - Exam Questions/Q042/src/Client.java
@@ -0,0 +1,22 @@
+/**
+ * Q042:
+ * Write a client-server program using UDP socket. 
+ * Develop a program to implement one way communication using 
+ * Connection-less programming techniques in Java.
+ */
+import java.io.*;  
+import java.net.*;  
+public class Client {  
+public static void main(String[] args) {  
+try{      
+    Socket s=new Socket("localhost",6666);  
+    DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    dout.writeUTF("Hello Server");  
+    dout.flush();  
+    dout.close();  
+    s.close();  
+}
+catch(Exception e)
+{   System.out.println(e);  }
+}  
+}  
---
+++ AJP - Exam Questions/Q042/src/Server.java
@@ -0,0 +1,22 @@
+/**
+ * Q042:
+ * Write a client-server program using UDP socket. 
+ * Develop a program to implement one way communication using 
+ * Connection-less programming techniques in Java.
+ */
+import java.io.*;  
+import java.net.*;  
+public class Server {  
+public static void main(String[] args){  
+try{  
+    ServerSocket ss=new ServerSocket(6666);  
+    Socket s=ss.accept();   
+    DataInputStream dis=new DataInputStream(s.getInputStream());  
+    String  str=(String)dis.readUTF();  
+    System.out.println("Client says: "+str);  
+    ss.close();  
+}
+catch(Exception e)
+{   System.out.println(e);  }  
+}  
+}  
