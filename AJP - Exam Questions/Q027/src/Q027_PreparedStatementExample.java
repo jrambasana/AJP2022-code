b05a6da8047938116e6af4018620d7bdcf29f69c
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-09-14 19:07:03 +0530

    Q027 added

---
+++ AJP - Exam Questions/Q027/src/Q027_PreparedStatementExample.java
@@ -0,0 +1,41 @@
+/**
+ * Q027:
+ * Write a program to insert a record in a table 
+ * using Prepared Statement Object. 
+ */
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+public class Q027_PreparedStatementExample {
+public static void main(String[] args) {
+    try {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection cn =      	     
+            DriverManager.getConnection
+            ("jdbc:mysql://localhost:3306/test", "root", "");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Employee ID: ");
+        String eid = sc.next();
+        System.out.print("Enter Employee Name: ");
+        String en = sc.next();
+        System.out.print("Enter Employee City: ");
+        String ec = sc.next();
+        System.out.print("Enter Employee Salary: ");
+        String es = sc.next();
+        PreparedStatement smt = cn.prepareStatement
+                ("insert into employees values(?,?,?,?)");
+        smt.setString(1, eid);
+        smt.setString(2, en);
+        smt.setString(3, ec);
+        smt.setString(4, es);
+        smt.executeUpdate();
+        System.out.println("Record Submitted....");
+        cn.close();
+    } 
+    catch (Exception e) 
+    {
+        System.out.println(e);
+    }
+}
+}
