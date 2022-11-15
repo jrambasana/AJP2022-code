57df7b8f0fa6a7e51aaa6572b4afd72d621b7ca5
Jatin Ambasana's Repository <jatinambasana@gmail.com>
2022-11-15 13:06:31 +0530

    Code updated

--- AJP-Lab-Programs/NetBeans/AJP-Lab-Chp06-Spring/Spring_Lab_Solution/SpringAOPDemo/src/A.java
+++ AJP-Lab-Programs/NetBeans/AJP-Lab-Chp06-Spring/Spring_Lab_Solution/SpringAOPDemo/src/A.java
@@ -1,9 +1,5 @@
 public class A {
     public void m(){
         System.out.println("This is actual business logic");
-    } 
-//    public String a(){
-//        return "AAAAAA";
-//    }
-            
+    }    
 }
--- AJP-Lab-Programs/NetBeans/AJP-Lab-Chp06-Spring/Spring_Lab_Solution/SpringAOPDemo/src/BeforeAdvisor.java
+++ AJP-Lab-Programs/NetBeans/AJP-Lab-Chp06-Spring/Spring_Lab_Solution/SpringAOPDemo/src/BeforeAdvisor.java
@@ -2,11 +2,11 @@ import java.lang.reflect.Method;
 import org.springframework.aop.AfterAdvice;
 import org.springframework.aop.MethodBeforeAdvice;
 
-    public class BeforeAdvisor implements MethodBeforeAdvice{
-    
-    public void before(Method method, Object[] args, Object target)throws Throwable {  
-        System.out.println("This is some additional concern BEFORE actual logic");  
-    }  
+public class BeforeAdvisor implements MethodBeforeAdvice{
+
+public void before(Method method, Object[] args, Object target)throws Throwable {  
+    System.out.println("This is some additional concern BEFORE actual logic");  
+}  
 
 //    public void afterReturning(Object o, Method method, Object[] os, Object o1)throws Throwable {  
 //        System.out.println("additional concern AFTER actual logic");  
